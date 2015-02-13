5a12f261af21e3191ffefb2a30b22ab84414c88c
Sulverus <sulverus@gmail.com>
2015-02-13 05:19:53 +0000

    setup.py updated

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def long_description():
 
 setup(
     name='debuilder',
-    version='0.3.1',
+    version='0.3.2',
     description="Debian packages build tool",
     long_description=long_description(),
     url='https://github.com/bigbes/debgitbuild',
