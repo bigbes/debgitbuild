a2077254d6b2ea51ceadd903b4a34cb5c9e464b7
Sulverus <sulverus@gmail.com>
2015-02-16 12:49:57 +0000

    Version bump

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def long_description():
 
 setup(
     name='debuilder',
-    version='0.3.3',
+    version='0.3.4',
     description="Debian packages build tool",
     long_description=long_description(),
     url='https://github.com/bigbes/debgitbuild',
